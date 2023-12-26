86c86c35f088cff88f0a98a10e965d2d63055692
Luar <luarrezende@gmail.com>
2023-12-26 14:53:51 -0300

    cria base para userPage e notfound

--- frontend/src/Router/index.js
+++ frontend/src/Router/index.js
@@ -1,11 +1,11 @@
 import { Route, Switch } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
+import UserPage from '../pages/UserPage';
 import NotFound from '../pages/NotFound';
 
 function Router() {
   return (
     <Switch>
-      <Route exact path="/" component={ LoginPage } />
+      <Route exact path="/" component={ UserPage } />
       <Route exact path="*" component={ NotFound } />
     </Switch>
   );
