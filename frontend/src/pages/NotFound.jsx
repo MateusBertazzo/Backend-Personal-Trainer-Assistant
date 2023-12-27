a741fc1bca337623a69f225fe1c7db06b2fa04c3
Luar <luarrezende@gmail.com>
2023-12-26 22:55:04 -0300

    cria página de notFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './style/notFound.css'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="notFoundContainer">
+      <h1 className="notFoundTitle">Página não encontrada</h1>
+      <p className="notFoundText">404</p>
+      <Link to="/" className='backToHomeLink'>Volte para a página inicial</Link>
+    </div>
   )
 }
 
