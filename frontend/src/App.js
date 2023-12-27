7efcccca870851dcd42a349b5bec200fd45773c3
Luar <luarrezende@gmail.com>
2023-12-27 14:20:14 -0300

    cria funcionalidade basica de login com alert personalizado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Router from './Router';
 
 function App() {
   return (
     <div>
       <Router />
+      <ToastContainer />
     </div>
   );
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,15 +1,61 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import context from '../context/MyContext';
+import { toast } from 'react-toastify';
 
 function Login() {
   const { setLoginState } = useContext(context);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const isEmailValid = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const isPasswordStrong = (password) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!isEmailValid(email) || !isPasswordStrong(password)) {
+      toast.error('Email ou senha não atendem aos requisitos');
+      return;
+    }
+
+    toast.success('Login efetuado com sucesso');
+  };
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Entrar</h1>
+
+      <input
+        type="text"
+        placeholder="E-mail"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
       <button
+        type="button"
+        onClick={handleSubmit}
+      >
+        Entrar
+      </button>
+
+      <button
+        type="button"
         onClick={() => setLoginState(false)}
       >
-        to Cadastro
+        Ainda não tenho conta
       </button>
     </div>
   )
