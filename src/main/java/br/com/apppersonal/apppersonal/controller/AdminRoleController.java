fbdba85065dcd16db31ea9f7449d4eaa0d66bbea
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-01-24 20:37:59 -0300

    implementa APIResponse na rota de ChanceRole

--- src/main/java/br/com/apppersonal/apppersonal/controller/AdminRoleController.java
+++ src/main/java/br/com/apppersonal/apppersonal/controller/AdminRoleController.java
@@ -2,8 +2,10 @@ package br.com.apppersonal.apppersonal.controller;
 
 import br.com.apppersonal.apppersonal.security.Role;
 import br.com.apppersonal.apppersonal.service.AdminRoleService;
+import br.com.apppersonal.apppersonal.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +16,29 @@ public class AdminRoleController {
 
     private final AdminRoleService adminRoleService;
 
+    private final ApiResponse apiResponse;
+
     @Autowired
-    public AdminRoleController(AdminRoleService adminRoleService) {
+    public AdminRoleController(AdminRoleService adminRoleService, ApiResponse apiResponse) {
         this.adminRoleService = adminRoleService;
+        this.apiResponse = apiResponse;
     }
 
     @PostMapping("/change-role/admin/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void changeRoleAdmin(@PathVariable Long id) {
-        adminRoleService.ChangeUserRole(id, Role.ADMIN);
+    public ResponseEntity<ApiResponse> changeRoleAdmin(@PathVariable Long id) {
+       return apiResponse.request(adminRoleService.ChangeUserRole(id, Role.ADMIN));
     }
 
     @PostMapping("/change-role/personal/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void changeRolePersonal(@PathVariable Long id) {
-        adminRoleService.ChangeUserRole(id, Role.PERSONAL);
+    public ResponseEntity<ApiResponse> changeRolePersonal(@PathVariable Long id) {
+       return apiResponse.request(adminRoleService.ChangeUserRole(id, Role.PERSONAL));
     }
 
     @PostMapping("/change-role/user/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void changeRoleUser(@PathVariable Long id) {
-        adminRoleService.ChangeUserRole(id, Role.USER);
+    public ResponseEntity<ApiResponse> changeRoleUser(@PathVariable Long id) {
+       return apiResponse.request(adminRoleService.ChangeUserRole(id, Role.USER));
     }
 }
