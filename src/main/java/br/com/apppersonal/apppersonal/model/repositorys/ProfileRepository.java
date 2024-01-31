025d0fbb32f498af38d60f70150c313cc168702b
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-01-31 17:50:31 -0300

    implementa verificação se conta é deletada ou não na rota profile

--- src/main/java/br/com/apppersonal/apppersonal/model/repositorys/ProfileRepository.java
+++ src/main/java/br/com/apppersonal/apppersonal/model/repositorys/ProfileRepository.java
@@ -2,7 +2,16 @@ package br.com.apppersonal.apppersonal.model.repositorys;
 
 import br.com.apppersonal.apppersonal.model.entitys.ProfileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProfileRepository extends JpaRepository<ProfileEntity, Long> {
     ProfileEntity findByUserId(Long id);
+
+    @Query("select p from ProfileEntity p where p.deleted = false")
+    List<ProfileEntity> findAllNotDeleted();
+
+    @Query("select p from ProfileEntity p where p.id = ?1 and p.deleted = false")
+    ProfileEntity findByIdAndNotDeleted(Long id);
 }
