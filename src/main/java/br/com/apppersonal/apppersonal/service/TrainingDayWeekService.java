c48bcfb92e9a82c9e6baff48cec0f1b01bbae95e
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-11 10:03:25 -0300

    feat - documenta trainingDayOfWeekService

--- src/main/java/br/com/apppersonal/apppersonal/service/TrainingDayWeekService.java
+++ src/main/java/br/com/apppersonal/apppersonal/service/TrainingDayWeekService.java
@@ -35,11 +35,19 @@ public class TrainingDayWeekService {
         this.exerciseRepository = exerciseRepository;
     }
 
+    /**
+     * Método para criar um treino
+     *
+     * @param   CreateTrainingDto createTrainingDto
+     * @return  ResponseEntity
+     */
     public ResponseEntity<?> createTrainingDayWeek(CreateTrainingDto createTrainingDto) {
         try {
 
             if (createTrainingDto.getDayOfWeek() == null) throw new ParameterNullException("Dia da semana não pode ser vazio");
+
             if (createTrainingDto.getExerciseEntityList() == null) throw new ParameterNullException("Exercícios não podem ser vazios");
+
             if (createTrainingDto.getUserId() == null) throw new ParameterNullException("Usuário não pode ser vazio");
 
             UserEntity user = userService.getUserById(createTrainingDto.getUserId());
@@ -47,6 +55,7 @@ public class TrainingDayWeekService {
             if (user == null) throw new UserNotFoundException("Usuário não encontrado");
 
             TrainingEntity trainingEntity = new TrainingEntity();
+
             trainingEntity.setUser(user);
             trainingEntity.setDayOfWeek(createTrainingDto.getDayOfWeek());
 
@@ -78,12 +87,18 @@ public class TrainingDayWeekService {
 
     }
 
+    /**
+     * Método para retornar os exercícios do treino pelo id
+     *
+     * @param   Long trainingId
+     * @return  ResponseEntity
+     */
     public ResponseEntity<?> getExerciseByTrainingId(Long trainingId) {
+
         try {
             if (trainingId == null) throw new ParameterNullException("Identificador do treino não informado");
 
             // verificar se o treino não está deletado
-
             Boolean isTrue = trainingRepository.isDeletedTraining(trainingId);
 
             if (!isTrue) throw new ExercisesNotFound("Treino não encontrado");
@@ -96,17 +111,21 @@ public class TrainingDayWeekService {
             List<TrainingExercicesDto> responseDtoList = new ArrayList<>();
 
             for (ExerciseEntity exerciseEntity : exerciseEntities) {
-                responseDtoList.add(new TrainingExercicesDto(
-                        exerciseEntity.getId(),
-                        exerciseEntity.getName(),
-                        exerciseEntity.getTraining().getId(),
-                        exerciseEntity.getTraining().getDayOfWeek(),
-                        exerciseEntity.getDescription(),
-                        exerciseEntity.getRepetition(),
-                        exerciseEntity.getWeight(),
-                        exerciseEntity.getRepose()
-                ));
+                responseDtoList
+                        .add(
+                                new TrainingExercicesDto(
+                                        exerciseEntity.getId(),
+                                        exerciseEntity.getName(),
+                                        exerciseEntity.getTraining().getId(),
+                                        exerciseEntity.getTraining().getDayOfWeek(),
+                                        exerciseEntity.getDescription(),
+                                        exerciseEntity.getRepetition(),
+                                        exerciseEntity.getWeight(),
+                                        exerciseEntity.getRepose()
+                                )
+                        );
             }
+
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(
