5d728bcb1573047e37b12ffe03e0f35c7da6e7e0
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-13 07:37:07 -0300

    fix na controller de EmailController

--- src/main/java/br/com/apppersonal/apppersonal/controller/EmailController.java
+++ src/main/java/br/com/apppersonal/apppersonal/controller/EmailController.java
@@ -4,6 +4,7 @@ import br.com.apppersonal.apppersonal.model.Dto.EmailRequestDto;
 import br.com.apppersonal.apppersonal.model.Dto.ResetPasswordForgotDto;
 import br.com.apppersonal.apppersonal.service.UserService;
 import br.com.apppersonal.apppersonal.utils.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +24,12 @@ public class EmailController {
     }
 
     @PostMapping("/send-email")
-    public void resetPasswordForgotRequest(@RequestBody EmailRequestDto emailRequest) {
-        apiResponse.request(userService.sendEmailForgotPasswordRequest(emailRequest.getTo()));
+    public ResponseEntity<ApiResponse> resetPasswordForgotRequest(@RequestBody EmailRequestDto emailRequest) {
+       return apiResponse.request(userService.sendEmailForgotPasswordRequest(emailRequest.getTo()));
     }
 
     @PostMapping("/reset-password")
-    public void resetPasswordForgot(@RequestBody ResetPasswordForgotDto resetPasswordForgotDto) {
-        apiResponse.request(userService.resetPasswordForgot(resetPasswordForgotDto));
+    public ResponseEntity<ApiResponse> resetPasswordForgot(@RequestBody ResetPasswordForgotDto resetPasswordForgotDto) {
+       return apiResponse.request(userService.resetPasswordForgot(resetPasswordForgotDto));
     }
 }
